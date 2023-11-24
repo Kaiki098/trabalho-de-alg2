8e5c9008e197f9fce97ee4597f3526f7179b6a45
Kaiki098 <127666620+Kaiki098@users.noreply.github.com>
2023-11-24 18:48:41 -0300

    Update trabalho_aquivoTexto.c
    adição de comentários

--- trabalho_aquivoTexto.c
+++ trabalho_aquivoTexto.c
@@ -7,11 +7,13 @@ int main(){
 	FILE *arq1, *arq2, *arq3;
 	char str_arq1[100], str_arq2[100];
 	int cont = 0;
-	
-	arq1 = fopen("Arquivo.txt", "r");
+
+	arq1 = fopen("Arquivo.txt", "r");//abre os arquivos para leitura
 	arq2 = fopen("Arquivo2.txt", "r");
-	arq3 = fopen("Arquivo3.txt", "w");
-	
+	arq3 = fopen("Arquivo3.txt", "w");//abre arquivo para escrita
+
+	/*pega cada nome do arq1 e compara com todos os nomes do arq2, se os nomes forem iguais
+	armazena em arq3 adiciona um no contador e vaia para o proximo nome do arq 1*/
 	while(fgets(str_arq1, 100, arq1) != NULL){
 		while(fgets(str_arq2, 100, arq2) != NULL){
 			if(strcmp(str_arq1, str_arq2) == 0){
@@ -22,11 +24,13 @@ int main(){
 		}
 		rewind(arq2);
 	}
-	
+
+	//fecha arquivos
 	fclose(arq1);
 	fclose(arq2);
 	fclose(arq3);
-	
+
+	//exibe mensagem com o numero de nomes copiados para arq3
 	printf("Foram encontrados %d nomes duplicados e entao copiados para o Arquivo3!\n", cont);
 	
 	system("pause");
